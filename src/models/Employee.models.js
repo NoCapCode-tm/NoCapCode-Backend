5f93db5c00e7ea7c38898f8ee18bbcba243731fe
Mohammad Ziya <ziya7376502028@gmail.com>
2026-07-24 19:37:23 +0530

    mail resend fixes

--- src/models/Employee.models.js
+++ src/models/Employee.models.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import atlasDb from "../database/atlasdb.js"
 const UserSchema = new mongoose.Schema({
-    name:{
+   name:{
         type:String,
         trim:true,
         required:true,
@@ -20,6 +20,12 @@ const UserSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"Role",
         default:null
+    },
+     email:{
+        type:String,
+        lowercase:true,
+        unique:true,
+        trim:true
     },
     Emails:{
         email:{
@@ -98,12 +104,12 @@ const UserSchema = new mongoose.Schema({
     phone:{
         permanent:{
         type:Number,
-        unique:true,
+        default:null,
         sparse:true
     },
      alternate:{
         type:Number,
-        unique:true,
+        default:null,
         sparse:true
     },
 },
@@ -124,20 +130,20 @@ const UserSchema = new mongoose.Schema({
         enum:["Manager","Human Resource","Intern","Administrator","Employee"],
         default:"Employee"
         },
-        Managerid:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Manager",
+        // Managerid:{
+        //     type:mongoose.Schema.Types.ObjectId,
+        //     ref:"Manager",
 
-        },
-        Hrid:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"HR",
+        // },
+        // Hrid:{
+        //     type:mongoose.Schema.Types.ObjectId,
+        //     ref:"HR",
             
-        }
+        // }
     },
     role:{
         type:String,
-        enum:["Frontend Developer","Backend Developer","Full Stack Developer","QA","UI/UX Designer","Devops","Manager"],
+        // enum:["Frontend Developer","Content Writer","Backend Developer","Full Stack Developer","QA","UI/UX Designer","Devops","Manager"],
         default:null
     },
     status:{
@@ -145,20 +151,20 @@ const UserSchema = new mongoose.Schema({
         enum:["Onboarding","Paid" , "Unpaid" ,"Full Time","Contractual"],
         default:"Onboarding"
     },
-    Projects:[
-        {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Project",
-        }
-    ],
-    Tasks:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Tasks"
-    }],
-    dailyreports:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Report"
-    }],
+    // Projects:[
+    //     {
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:"Project",
+    //     }
+    // ],
+    // Tasks:[{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:"Tasks"
+    // }],
+    // dailyreports:[{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:"Report"
+    // }],
 
     managerAssigned:{
         type:mongoose.Schema.Types.ObjectId,
@@ -443,10 +449,16 @@ acknowledge:{
     default:"",
 },
 
+policyhandbook:{
+    type:Boolean,
+    default:false
+},
+
 deleted:{
   type:Boolean,
   default:false
 }
+
 
 },{timestamps:true})
 
